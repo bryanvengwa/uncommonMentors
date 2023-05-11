f21258711605c3f3fa72a64106ff4c414b6c51e4
bryanvengwa <119483672+bryanvengwa@users.noreply.github.com>
2023-05-11 21:36:27 +0200

    updating queries

--- js/script.js
+++ js/script.js
@@ -1,53 +1,92 @@
-hamburger = document.querySelector(".hamburger");
-hamburger.onclick = function () {
-	navBar = document.querySelector(".nav-bar");
-	navBar.classList.toggle("active");
-};
+"use strict";
+const navItems = document.querySelectorAll(".nav-item");
+const navBar = document.querySelector("nav");
+const links = [...document.querySelectorAll(".link")];
+const images = document.querySelector(".images");
+const mentors = [...document.querySelectorAll(".mentors")];
+const courses = [...document.querySelectorAll(".courses")];
+const courseTab = document.querySelector(".courseTab");
+const mentorH = document.getElementById("mentor-h1");
+const sign = document.querySelector(".sign");
+const signUp = document.querySelector(".sign-up");
+const closeModal = document.querySelector(".close-modal");
+const logInBtn = document.querySelector(".log-in");
+const signUpBtn = document.querySelector(".sign-Up-btn");
+const loggingIn = document.querySelector(".logging-in");
+const closeModals = document.querySelector(".close-modals");
 
-$(".services-wrapper").slick({
-	dots: false,
-	infinite: false,
-	// arrows:false,
-	speed: 300,
-	slidesToShow: 3,
-	slidesToScroll: 3,
-	pagination: {
-		el: ".swiper-pagination",
-		clickable: true,
-	},
-	responsive: [
-		{
-			breakpoint: 1024,
-			settings: {
-				slidesToShow: 3,
-				slidesToScroll: 3,
-				infinite: true,
-				dots: true,
-			},
-		},
-		{
-			breakpoint: 900,
-			settings: {
-				slidesToShow: 2,
-				slidesToScroll: 1,
-				arrows: false,
-			},
-		},
-		{
-			breakpoint: 600,
-			settings: {
-				slidesToShow: 2,
-				slidesToScroll: 1,
-				arrows: false,
-			},
-		},
-		{
-			breakpoint: 480,
-			settings: {
-				slidesToShow: 1,
-				slidesToScroll: 1,
-				arrows: false,
-			},
-		},
-	],
+// const rect = mentorH.getBoundingClientRect();
+// console.log(rect);
+// const mentorX = rect.x;
+// const mentorY = rect.y;
+
+let currentHref;
+
+
+
+navBar.addEventListener("click", function (event) {
+	event.preventDefault();
+	// console.log('clicked!!!')
+	links.forEach((link) => {
+		link.classList.remove("active-link");
+	});
+	// console.log(event.target);
+	if (event.target.classList.contains("link")) {
+		
+		currentHref = event.target.href;
+		window.location.href = currentHref;
+	}else if(event.target.classList.contains('sign')){
+		signUp.classList.toggle('hide')
+	
+
+	}
+});
+
+closeModal.addEventListener('click', function(event){
+	event.preventDefault()
+
+	loggingIn.classList.add("hide");
+	
+	
+})
+closeModals.addEventListener("click", function (event) {
+	event.preventDefault();
+	signUp.classList.add("hide");
+});
+
+logInBtn.addEventListener('click', function(event){
+	event.preventDefault();
+	loggingIn.classList.remove("hide");
+	signUp.classList.toggle("hide");
+	
+})
+
+
+signUpBtn.addEventListener('click', function(event){
+	event.preventDefault();
+	console.log("clicked!!!");
+	loggingIn.classList.add("hide");
+	signUp.classList.toggle("hide");
+
+});
+
+
+// mentors section tab
+images.addEventListener("click", function (event) {
+	if (event.target.classList.contains("mentors")) {
+		mentors.forEach(function (mentor) {
+			mentor.classList.remove("active");
+		});
+		event.target.classList.add("active");
+	}
+});
+
+// courses tab at mentors page
+courseTab.addEventListener("click", function (event) {
+	if (event.target.classList.contains("courses")) {
+		courses.forEach(function (course) {
+			course.classList.remove("active-course");
+		});
+		event.target.classList.add("active-course");
+	}
 });
